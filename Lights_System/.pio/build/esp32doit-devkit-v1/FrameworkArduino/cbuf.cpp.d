65e9a2f863ac6eecf7519307a68ee59482e205b6
ItIsMeJuly <70228174+ItIsMeJuly@users.noreply.github.com>
2022-05-23 01:23:20 +0200

    build files for Lights System

---
+++ Lights_System/.pio/build/esp32doit-devkit-v1/FrameworkArduino/cbuf.cpp.d
@@ -0,0 +1,3 @@
+.pio/build/esp32doit-devkit-v1/FrameworkArduino/cbuf.cpp.o: \
+ C:/Users/Start/.platformio/packages/framework-arduinoespressif32@src-537c58760dafe7fcc8a1d9bbcf00b6f6/cores/esp32/cbuf.cpp \
+ C:/Users/Start/.platformio/packages/framework-arduinoespressif32@src-537c58760dafe7fcc8a1d9bbcf00b6f6/cores/esp32/cbuf.h
